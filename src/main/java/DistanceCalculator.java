c25afd844c58f8fa47ec3646ffe68afd63c511c6
k-r-3 <rke19922802@gmail.com>
2021-08-25 02:24:20 +0500

    core features

--- src/main/java/DistanceCalculator.java
+++ src/main/java/DistanceCalculator.java
@@ -17,19 +17,111 @@ public class DistanceCalculator {
         System.out.println(stationList.size());
     }
 
-    public Map<Integer, List<Integer>> calculateDistance(List<Integer> stations) {
+    //считаем расстояния между станциями
+    Map<Integer, List<Integer>> calculateDistance(List<Integer> stations) {
         Map<Integer, List<Integer>> rsl = new HashMap<>();
         for (int i = 0; i < stations.size() - 1; i++) {
+            List<Integer> distances = rsl.get(i);
             for (int j = i + 1; j < stations.size(); j++) {
-                List<Integer> distances = rsl.get(i);
                 if (distances == null) {
                     distances = new ArrayList<>();
                 }
                 distances.add(Math.abs(stations.get(i) - stations.get(j)));
-                rsl.put(i, (distances == null ? new ArrayList<>() : distances));
+            }
+            rsl.put(i, (distances));
+        }
+        rsl.put(stations.size() - 1, List.of(0));
+        return rsl;
+
+    }
+
+    //объединяем интервалы в серии
+    public Map<Integer, Integer> calculateDuplicates(Map<Integer, List<Integer>> in) {
+        Map<Integer, Integer> rsl = new HashMap<>();
+        for (int i = 0; i < in.size(); i++) {
+            List<Integer> inner = in.get(i);
+            for (int j = 0; j < inner.size(); j++) {
+                Integer amount = rsl.get(inner.get(j));
+                rsl.put(inner.get(j), (Objects.isNull(amount) ? 1 : amount + 1));
             }
         }
         return rsl;
+    }
 
+    //оставляем серии с наибольшими отрезками
+    public Map<Integer, Integer> deleteSmallestDuplicate(Map<Integer, Integer> in) {
+        Map<Integer, Integer> rsl = new HashMap<>();
+        for (Map.Entry<Integer, Integer> entry : in.entrySet()) {
+            int amount = entry.getValue();
+            Integer station = rsl.get(amount);
+            rsl.put(amount, (Objects.isNull(station) ? entry.getKey() : Math.max(station, entry.getKey())));
+        }
+        return rsl;
+    }
+
+    //оставляем серию с колличеством отрезков наиболее близким к колличеству,которое нужно оставить
+    public Map<Integer, Integer> deleteStations(int amount, Map<Integer, Integer> in) {
+        Map<Integer, Integer> rsl = new HashMap<>();
+        if (in.size() == 1) {
+            rsl.put(amount, in.values().stream().findFirst().get());
+        } else {
+            Integer element = in.get(amount);
+            int key = amount;
+            if (Objects.isNull(element)) {
+                List<Integer> keys = new ArrayList<>(in.keySet());
+                Collections.sort(keys);
+                int temp;
+                for (Integer i : keys) {
+                    if (i < amount) {
+                        key = i;
+                    }
+                }
+            }
+            rsl.put(key, in.get(key));
+        }
+        return rsl;
+    }
+
+    //сопоставляем отрезок со станциями, и в результат пишем те пары индексов, между которыми этот отрезок
+    public Set<Integer> sortStations(Map<Integer, List<Integer>> allStations, Map<Integer, Integer> distance) {
+        Set<Integer> rsl = new HashSet<>();
+        int pattern = distance.values()
+                .stream()
+                .findFirst()
+                .get();
+        for (int i = 0; i < allStations.size(); i++) {
+            List<Integer> inner = allStations.get(i);
+            for (int j = 0; j < inner.size(); j++) {
+                if (inner.get(j) == pattern) {
+                    rsl.add(i);
+                    rsl.add(i + j + 1);
+                }
+            }
+        }
+        return rsl;
+    }
+
+    public String receipt(List<Integer> firstLine, List<Integer> secondLine) {
+        int stationsAmount = firstLine.get(0);
+        int amountForDelete = firstLine.get(1);
+        Map<Integer, List<Integer>> distances = calculateDistance(secondLine);
+        Map<Integer, Integer> duplicates = calculateDuplicates(distances);
+        Map<Integer, Integer> longestDuplicate = deleteSmallestDuplicate(duplicates);
+        Map<Integer, Integer> leftDistance = deleteStations((distances.size() - amountForDelete), longestDuplicate);
+        Set<Integer> stationIndexes = sortStations(distances, leftDistance);
+        List<Integer> stations = new ArrayList();
+        for (Map.Entry<Integer, List<Integer>> entry : distances.entrySet()) {
+            int stationNumber = entry.getKey();
+            if (!stationIndexes.contains(stationNumber)) {
+                stations.add(stationNumber + 1);
+            }
+        }
+        stations = stations.isEmpty() ? List.of(1) : stations;
+        return String.format("%s\n%s",
+                leftDistance.values().stream()
+                        .findFirst()
+                        .get(),
+                stations
+        );
     }
 }
--- src/test/java/DistanceCalculatorTest.java
+++ src/test/java/DistanceCalculatorTest.java
@@ -1,9 +1,6 @@
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
@@ -18,4 +15,151 @@ public class DistanceCalculatorTest {
         assertThat(rsl.toString(), is ("{0=[8, 43, 31, 54], 1=[35, 23, 46], 2=[12, 11], 3=[23]}"));
     }
 
+//    @Test
+//    public void when5StationsThen2Duplicates() {
+//        DistanceCalculator calculator = new DistanceCalculator();
+//        Map<Integer, List<Integer>> in = new HashMap<>();
+//        in.put(0, Arrays.asList(8, 43, 31, 54));
+//        in.put(1, Arrays.asList(35, 23, 46));
+//        in.put(2, Arrays.asList(12, 11));
+//        in.put(3, Arrays.asList(23));
+//        Map<Integer, Integer> rsl = calculator.calculateDuplicates(in);
+//        System.out.println(rsl);
+//        assertThat(rsl.get(35), is(1));
+//        assertThat(rsl.get(23), is(2));
+//    }
+
+//    @Test
+//    public void whenManyDuplicateThenLeftGreatest() {
+//        DistanceCalculator calculator = new DistanceCalculator();
+//        Map<Integer, Integer> in = new HashMap<>();
+//        in.put(35, 1);
+//        in.put(12, 1);
+//        in.put(30, 1);
+//        in.put(39, 1);
+//        in.put(23, 2);
+//        in.put(40, 2);
+//        in.put(64, 3);
+//        in.put(4, 3);
+//        Map<Integer, Integer> rsl = calculator.deleteSmallestDuplicate(in);
+//        System.out.println(rsl);
+//        assertThat(rsl, is(Map.of(1, 39, 2, 40, 3, 64)));
+//    }
+
+    @Test
+    public void when4StationLeft() {
+        DistanceCalculator calculator = new DistanceCalculator();
+        Map<Integer, Integer> in = new HashMap<>();
+        in.put(1, 39);
+        in.put(2, 40);
+        in.put(3, 60);
+        Map<Integer, Integer> rsl = calculator.deleteStations(4, in);
+        assertThat(rsl, is(Map.of(3, 60)));
+    }
+
+    @Test
+    public void findStations() {
+        DistanceCalculator calculator = new DistanceCalculator();
+        var distance = Map.of(2, 23);
+        Map<Integer, List<Integer>> allStations = Map.of(0, List.of(8, 43, 31, 54),1, List.of(35, 23, 46),
+                2, List.of(12, 11), 3, List.of(23));
+        Set<Integer> leftStations = calculator.sortStations(allStations, distance);
+        System.out.println(leftStations);
+    }
+
+    @Test
+    public void when5StationsThen2Duplicates() {
+        DistanceCalculator calculator = new DistanceCalculator();
+        Map<Integer, List<Integer>> in = new HashMap<>();
+        in.put(0, Arrays.asList(8, 43, 31, 54));
+        in.put(1, Arrays.asList(35, 23, 46));
+        in.put(2, Arrays.asList(12, 11));
+        in.put(3, Arrays.asList(23));
+        Map<Integer, Integer> rsl = calculator.calculateDuplicates(in);
+        System.out.println(rsl);
+        assertThat(rsl.get(35), is(1));
+        assertThat(rsl.get(23), is(2));
+    }
+
+    @Test
+    public void whenManyIntervalsThenLeftGreatest() {
+        DistanceCalculator calculator = new DistanceCalculator();
+        Map<Integer, Integer> in = new HashMap<>();
+        in.put(35, 1);
+        in.put(12, 1);
+        in.put(30, 1);
+        in.put(39, 1);
+        in.put(23, 2);
+        in.put(40, 2);
+        in.put(64, 3);
+        in.put(4, 3);
+        Map<Integer, Integer> rsl = calculator.deleteSmallestDuplicate(in);
+        System.out.println(rsl);
+        assertThat(rsl, is(Map.of(1, 39, 2, 40, 3, 64)));
+    }
+
+    @Test
+    public void whenAmountStationLeft() {
+        DistanceCalculator calculator = new DistanceCalculator();
+        Map<Integer, Integer> in = new HashMap<>();
+        in.put(1, 39);
+        in.put(2, 40);
+        in.put(3, 60);
+        Map<Integer, Integer> rslFor3 = calculator.deleteStations(4, in);
+        Map<Integer, Integer> rslFor4 = calculator.deleteStations(3, in);
+        assertThat(rslFor3, is(Map.of(3, 60)));
+        assertThat(rslFor4, is(Map.of(3, 60)));
+    }
+
+    @Test
+    public void whenSortStations() {
+        DistanceCalculator calculator = new DistanceCalculator();
+        var distance = Map.of(2, 23);
+        Map<Integer, List<Integer>> allStations = Map.of(0, List.of(8, 43, 31, 54),1, List.of(35, 23, 46),
+                2, List.of(12, 11), 3, List.of(23));
+        Set<Integer> leftStations = calculator.sortStations(allStations, distance);
+        assertThat(leftStations, is(Set.of(1, 3, 4)));
+    }
+
+    @Test
+    public void whenInput5And3ThenReceipt() {
+        DistanceCalculator calculator = new DistanceCalculator();
+        String rsl = calculator.receipt(List.of(5, 3), List.of(12, 96, 6, 34, 73));
+        System.out.println(rsl);
+    }
+
+    @Test
+    public void whenInput6And3ThenReceipt() {
+        DistanceCalculator calculator = new DistanceCalculator();
+        String rsl = calculator.receipt(List.of(6, 3), List.of(12, 15, 66, 24, 120, 53));
+        System.out.println(rsl);
+    }
+
+    @Test
+    public void whenInput4And1ThenReceipt() {
+        DistanceCalculator calculator = new DistanceCalculator();
+        String rsl = calculator.receipt(List.of(4, 1), List.of(40, 80, 50, 60));
+        System.out.println(rsl);
+    }
+
+    @Test
+    public void whenInput4And2ThenReceipt() {
+        DistanceCalculator calculator = new DistanceCalculator();
+        String rsl = calculator.receipt(List.of(4, 2), List.of(40, 82, 8, 71));
+        System.out.println(rsl);
+    }
+
+    @Test
+    public void whenInput3And1ThenReceipt() {
+        DistanceCalculator calculator = new DistanceCalculator();
+        String rsl = calculator.receipt(List.of(3, 1), List.of(2, 2, 2));
+        System.out.println(rsl);
+    }
+
+    @Test
+    public void whenInput5And2ThenReceipt() {
+        DistanceCalculator calculator = new DistanceCalculator();
+        String rsl = calculator.receipt(List.of(5, 2), List.of(2, 12, 8, 8, 22));
+        System.out.println(rsl);
+    }
 }
